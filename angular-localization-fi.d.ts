e6b46e2e5d8c89cc9d8159e7ad5f0fd6f8f2b169
Steve Troppoli <stroppoli@tims.com>
2021-04-05 16:14:10 -0400

    more types

--- angular-localization-fi.d.ts
+++ angular-localization-fi.d.ts
@@ -3,3 +3,14 @@ export interface Locale {
     getLocale(): string;
     getString(key: string, params?: string | string[]): string;
 }
+
+export type LocaleNames = Language[];
+export interface Language {
+    locale: string;
+    language: string;
+}
+
+export interface LocaleEvents {
+    readonly resourceUpdates: string;
+    readonly localeChanges: string;
+};
